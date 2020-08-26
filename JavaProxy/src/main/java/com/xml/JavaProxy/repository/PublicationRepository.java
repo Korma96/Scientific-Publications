c43e9d82ace5da612dd15102192da2472acfb1a9
Marko Radovic <markoradovic996@gmail.com>
2020-08-26 17:50:57 +0200

    fix development

--- JavaProxy/src/main/java/com/xml/JavaProxy/repository/PublicationRepository.java
+++ JavaProxy/src/main/java/com/xml/JavaProxy/repository/PublicationRepository.java
@@ -3,20 +3,9 @@ package com.xml.JavaProxy.repository;
 import com.xml.JavaProxy.helper.XUpdateTemplate;
 import com.xml.JavaProxy.model.Publication;
 import com.xml.JavaProxy.model.Section;
-import com.xml.JavaProxy.util.MyNamespaceMapper;
 import com.xml.JavaProxy.util.XmlUtility;
 import org.springframework.stereotype.Repository;
 
-import javax.xml.XMLConstants;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.PropertyException;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import java.io.File;
-import java.io.StringReader;
-import java.io.StringWriter;
 
 @Repository
 public class PublicationRepository extends BaseRepository {
